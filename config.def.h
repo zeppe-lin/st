ea621a96380d338edd556272c31981e1eecc8e44
Alexandr Savca <alexandr.savca89@gmail.com>
2024-10-10 04:51:31 +0300

    config.def.h: set gruvbox theme by default

--- config.def.h
+++ config.def.h
@@ -93,42 +93,35 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence).
+ * Gruvbox theme by default.
  */
 static const char *colorname[] = {
-  /* 8 normal colors. */
-  "black",
-  "red3",
-  "green3",
-  "yellow3",
-  "blue2",
-  "magenta3",
-  "cyan3",
-  "gray90",
+  /* 8 normal colors */
+  [0] = "#32302f", /* hard contrast: #1d2021 / medium: #282828 / soft: #32302f */
+  [1] = "#cc241d", /* red     */
+  [2] = "#98971a", /* green   */
+  [3] = "#d79921", /* yellow  */
+  [4] = "#458588", /* blue    */
+  [5] = "#b16286", /* magenta */
+  [6] = "#689d6a", /* cyan    */
+  [7] = "#a89984", /* white   */
 
-  /* 8 bright colors. */
-  "gray50",
-  "red",
-  "green",
-  "yellow",
-  "#5c5cff",
-  "magenta",
-  "cyan",
-  "white",
-
-  [255] = 0,
-
-  /* More colors can be added after 255 to use with DefaultXX. */
-  "#cccccc",
-  "#555555",
-  "gray90",
-  "black"
+  /* 8 bright colors */
+  [8]  = "#928374", /* black   */
+  [9]  = "#fb4934", /* red     */
+  [10] = "#b8bb26", /* green   */
+  [11] = "#fabd2f", /* yellow  */
+  [12] = "#83a598", /* blue    */
+  [13] = "#d3869b", /* magenta */
+  [14] = "#8ec07c", /* cyan    */
+  [15] = "#ebdbb2", /* white   */
 };
 
 /* Default colors (colorname index).
  */
-unsigned int defaultfg = 258;          /* foreground color     */
-unsigned int defaultbg = 259;          /* background color     */
-static unsigned int defaultcs = 256;   /* cursor color         */
+unsigned int defaultfg = 15;           /* foreground color     */
+unsigned int defaultbg = 0;            /* background color     */
+static unsigned int defaultcs = 15;    /* cursor color         */
 static unsigned int defaultrcs = 257;  /* reverse cursor color */
 
 /*********************************************************************
